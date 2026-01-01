6bbd72b622ec40ed99d4a03549643553b057910e
Nagendra <nag.ch922@gmail.com>
2026-01-01 16:24:10 +0530

    implemented service class

--- src/main/java/com/chakriIT/service/UserService.java
+++ src/main/java/com/chakriIT/service/UserService.java
@@ -1,5 +1,29 @@
 package com.chakriIT.service;
 
-public interface UserService {
+import java.util.List;
 
+import com.chakriIT.dto.CityDto;
+import com.chakriIT.dto.CountryDto;
+import com.chakriIT.dto.QuoteApiResponseDto;
+import com.chakriIT.dto.ResetPwdDto;
+import com.chakriIT.dto.StateDto;
+import com.chakriIT.dto.UserDto;
+
+public interface UserService {
+	
+	public List<CountryDto> getCountries();
+	
+	public List<StateDto> getStates(Integer countryId);
+	
+	public List<CityDto> getCities(Integer stateId);
+	
+	public boolean isEmailUnique(String email);
+	
+	public boolean register(UserDto userDto);
+	
+	public UserDto login(String email, String pwd);
+	
+	public boolean resetPwdd(ResetPwdDto resetPwdDto);
+	
+	public QuoteApiResponseDto getQuote();
 }
